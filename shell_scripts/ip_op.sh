d41de73766150383b6baddb777b81a52771ea73d
Saikumar-Logisa <saikumar12234@gmail.com>
2025-06-06 00:21:32 +0530

    adding input output shell script

---
+++ shell_scripts/ip_op.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo this script will take input from user and prints the same, Lets begin with it.
+
+echo
+
+echo Hey, my name is Sai, what is your name btw?
+
+echo
+
+read name
+
+echo Hello Mr/Mrs $name + how are you doing today?
+
+read message
+
+echo Glad to hear that your day is going $message
