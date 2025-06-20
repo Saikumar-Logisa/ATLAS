268b488d7a0987d9b32c3bea8b5b46172ecc299e
Saikumar-Logisa <saikumar12234@gmail.com>
2025-06-20 13:18:31 +0530

    Adding Multithreading demo with Runnable Interface and Thread class

--- Java codes/Day_10/Task003.java
+++ Java codes/Day_10/Task003.java
@@ -41,3 +41,26 @@ class RunnableDemo implements Runnable {
         }
     }
 }
+
+/*
+output:
+
+Creating Thread-1
+Starting Thread-1
+Creating Thread-2
+Starting Thread-2
+Running Thread-1
+Running Thread-2
+Thread: Thread-1, 4
+Thread: Thread-2, 4
+Thread: Thread-1, 3
+Thread: Thread-2, 3
+Thread: Thread-1, 2
+Thread: Thread-2, 2
+Thread: Thread-1, 1
+Thread: Thread-2, 1
+Thread Thread-1 exiting.
+Thread Thread-2 exiting.
+
+
+*/
